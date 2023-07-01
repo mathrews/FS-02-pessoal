5a57cc355f451a236e76f5a499a3daf5349c60e0
mathrews <mateusps4gg@gmail.com>
2023-07-01 11:49:47 -0300

    aula 02 unid01 mod02

---
+++ modulo2/unidade1/aula2/anot.md
@@ -0,0 +1,15 @@
+# Laços de Repetição
+
+- () = argumentos e parametros da função, ou simplesmente, função anonima
+
+- laços de repetição = arrays
+
+- array = [] => sempre começa na posição 0
+
+- economiza memória, tempo e processamento
+
+- template string = ``
+
+- for = para
+
+- programação procidural = execução imediata
---
+++ modulo2/unidade1/aula2/script.js
@@ -0,0 +1,40 @@
+// let alunos = ['Davi', 'Mateus', 'vitor'];
+
+let diasPorMes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+for(let i = 1; i <= diasPorMes[0]; i++){
+    dia.innerHTML += `<option>${i}</option>`;
+}
+
+let meses = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+];
+
+for(let i = 0; i < meses.length; i++){
+    mes.innerHTML += `<option value="${i}">${meses[i]}</option>`;
+} 
+
+mes.onchange = function(){
+    dia.innerHTML = '';
+    for(let i = 1; i <= diasPorMes[mes.value]; i++){
+        dia.innerHTML += `<option>${i}</option>`;
+    }
+}
+
+
+let data = new Date(); //pega a data do server
+
+for(let i = data.getFullYear(); i > (data.getFullYear() - 101); i--){
+    ano.innerHTML += `<option>${i}</option>`;
+}
