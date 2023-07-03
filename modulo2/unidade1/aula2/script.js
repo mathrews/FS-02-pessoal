171cf071acc137393397077f9ffa2663e379bbd2
mathrews <mateusps4gg@gmail.com>
2023-07-03 16:00:28 -0300

    função de ano bissexto - feature 1

--- modulo2/unidade1/aula2/script.js
+++ modulo2/unidade1/aula2/script.js
@@ -32,9 +32,26 @@ mes.onchange = function(){
     }
 }
 
-
 let data = new Date(); //pega a data do server
+
+// let anoBi = for(let i = data.getFullYear(); i > ((data.getFullYear() - 101) % 4 === 0); i--){
+
+// }
 
 for(let i = data.getFullYear(); i > (data.getFullYear() - 101); i--){
     ano.innerHTML += `<option>${i}</option>`;
 }
+
+let diasPorMesBi = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+// ano.onchange = function(){
+//     if () {
+//         dia.innerHTML = '';
+//         for(let i = 1; i <= diasPorMesBi[mes.value]; i++){
+//             dia.innerHTML += `<option>${i}</option>`;
+//         }
+//         console.log('Esse ano é bissexto');
+//     } else {
+//         console.log('Esse ano não é bissexto');
+//     }
+// }
