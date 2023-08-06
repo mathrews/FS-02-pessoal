cd1f1aa3562c3b3b7655bb0f85716ad00b3e9f32
mathrews <mateusps4gg@gmail.com>
2023-08-06 09:17:45 -0300

    aula 03 unid 2 mod 2

---
+++ modulo2/unidade2/aula3/anot.md
@@ -0,0 +1,3 @@
+# Array methods e string methods:
+
+- Ler W3Schools array methods e string methods.
---
+++ modulo2/unidade2/aula3/js/script.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let = fruits.shift();
+
+const novoFruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let = novofruits.unshift("Lemon");
