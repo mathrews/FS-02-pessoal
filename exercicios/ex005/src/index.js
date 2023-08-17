9549733d77a20d917dd8a7fb53436437f7af3be0
mathrews <mateusps4gg@gmail.com>
2023-08-17 14:16:06 -0300

    api viacep funcionando corretamente

---
+++ exercicios/ex005/src/index.js
@@ -0,0 +1,37 @@
+let listagem = [];
+
+const buscarCep = () => {
+    if (campoCep.value.length == 0) {
+        res.innerHTML = `Coloque seu cep corretamente.`
+    }
+    try {
+        fetch(`https://viacep.com.br/ws/${campoCep.value}/json/`)
+        .then(res => res.json())
+        .then(item => {
+            listagem[0] = item;
+            mostrarLoc(listagem);
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const mostrarLoc = (lista) => {
+    res.innerHTML = '';
+    lista.map(item => {
+        res.innerHTML = `
+                <br>
+                <div>
+                zipcode: ${item.cep}
+                </div>
+                <h4>bairro: ${item.bairro}</h4>
+                <h5>localidade: ${item.localidade}</h5>
+                <p>DDD: ${item.ddd}</p>
+                <h6>UF: ${item.uf}</h6>
+        `;
+    });
+}
+
+btn.onclick = () => {
+    buscarCep()
+}
