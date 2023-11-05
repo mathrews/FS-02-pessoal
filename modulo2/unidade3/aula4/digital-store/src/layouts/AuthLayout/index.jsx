fe97b30ec7e9f3ddd982031c269743c741c48101
mathrews <mateusps4gg@gmail.com>
2023-11-05 17:09:24 -0300

    vercel deploy

--- modulo2/unidade3/aula4/digital-store/src/layouts/AuthLayout/index.jsx
+++ modulo2/unidade3/aula4/digital-store/src/layouts/AuthLayout/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import HeaderAuth from "../../components/HeaderAuth";
-import Footer from "../../components/footer";
+import Footer from "../../components/Footer";
 
 const AuthLayout = () => {
     return (
